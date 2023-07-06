03c9a454649a3f29d39e75f06ad312e736214131
Pushp Vashisht <pushptyagi1@gmail.com>
2023-07-06 01:13:50 +0100

    Renamed files to make tests modules for unit testing

--- sliding-window/find_maximum_in_sliding_window.py
+++ sliding-window/find_maximum_in_sliding_window.py
@@ -14,19 +14,22 @@ Array: -4 2 -5 3 6
 Output: 2 3 6
 """
 
+"""
 # My 1st approach and naive approach O(len(nums) - window_size * window_size) or O((n-k)*k)
 def find_max_sliding_window(nums, window_size):
     window_start, res_arr = 0, []
     for window_end in range(len(nums)):
         if window_end - window_start == window_size - 1:
-            res_arr.append(max(nums[window_start:window_end+1]))
+            res_arr.append(max(nums[window_start : window_end + 1]))
             window_start += 1
     return res_arr
+"""
 
 # Ideal solution using deque O(n)
 
 # Importing doubly ended queue
 from collections import deque
+
 
 def find_max_sliding_window(nums, window_size):
     res_arr = []
@@ -63,28 +66,3 @@ def find_max_sliding_window(nums, window_size):
         res_arr.append(nums[deq[0]])
 
     return res_arr
-
-# Driver code
-def main():
-    target_list = [3, 3, 3, 3, 2, 4, 3, 2, 3, 18]
-    nums_list = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                 [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-                 [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-                 [1, 5, 8, 10, 10, 10, 12, 14, 15, 19, 19, 19, 17, 14, 13, 12, 12, 12, 14, 18, 22, 26, 26, 26, 28, 29, 30],
-                 [10, 6, 9, -3, 23, -1, 34, 56, 67, -1, -4, -8, -2, 9, 10, 34, 67],
-                 [4, 5, 6, 1, 2, 3],
-                 [9, 5, 3, 1, 6, 3],
-                 [2, 4, 6, 8, 10, 12, 14, 16],
-                 [-1, -1, -2, -4, -6, -7],
-                 [4, 4, 4, 4, 4, 4]]
-
-    for i in range(len(nums_list)):
-        print(i + 1, ". Original array:\t", nums_list[i], sep="")
-        print("Window size:\t\t", target_list[i])
-        print("\n Max:\t\t",
-              find_max_sliding_window(nums_list[i], target_list[i]))
-        print("-"*100)
-
-
-if __name__ == '__main__':
-    main()
